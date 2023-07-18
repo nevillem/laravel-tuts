bbecc6668557ab26d1bc13c4b789549cbd401e60
nevillem <nevillemwije@gmail.com>
2023-07-19 00:34:28 +0300

    updated index blade

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,27 +1,15 @@
 @extends('layouts.layout')
+
 @section('content')
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <div class="content">
-            <img src="/img/farmer.png" alt="farmer"/>
-         <div class="title m-b-m-d"> 
-         PIZZA LIST
-         </div>
-         <p>{{$name}}</p>
+<div class="wrapper pizza-index">
+  <h1>Pizza Orders</h1>
+  @foreach($pizzas as $pizza)
+    <div class="pizza-item">
+      <img src="/img/pizza.png" alt="pizza icon">
+      <h4><a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }}</a></h4>
+    </div>
+  @endforeach
 
-          <!-- @for($i=0; $i < 5; $i++)
-          <p>the value of i is {{$i}}</p>
-          @endfor -->
-          <!-- @for($i=0; $i < count($pizzas); $i++)
-          <p>the value of i is {{$pizzas[$i]['type']}}</p>
-          @endfor -->
+</div>
 
-          @foreach($pizzas as $pizza)
-          <div>
-             {{ $pizza->index}} -{{ $pizza->type }} - {{ $pizza->base }}- {{ $pizza->price }}
-
-            </div>
-          @endforeach
-        </div>
-        </div>
-        @endsection
-
+@endsection
